ed92347685cf0041f15da4ec211b1200e382e7e5
devsko <devsko@outlook.com>
2023-01-26 00:18:30 +0100

    .

--- DiCor/VR.Values.cs
+++ DiCor/VR.Values.cs
@@ -9,11 +9,11 @@ namespace DiCor
             => (_value, isQueryContext) switch
             {
                 (0x4541, false) => new ValueTable<AEValue<IsNotQueryContext>>(),
-                (0x4541, true)  => new ValueTable<AEValue<IsQueryContext>>(),
-                (0x5351, _)     => new ValueTable<ASValue>(),
+                (0x4541, true) => new ValueTable<AEValue<IsQueryContext>>(),
+                (0x5351, _) => new ValueTable<ASValue>(),
                 (0x4144, false) => new ValueTable<DAValue<IsNotQueryContext>>(),
-                (0x4144, true)  => new ValueTable<DAValue<IsQueryContext>>(),
-                _               => throw new NotImplementedException(),
+                (0x4144, true) => new ValueTable<DAValue<IsQueryContext>>(),
+                _ => throw new NotImplementedException(),
             };
 
         internal AbstractValue CreateValue<T>(T content, bool isQueryContext)
@@ -21,11 +21,11 @@ namespace DiCor
             return (_value, isQueryContext) switch
             {
                 (0x4541, false) => CreateValue<AEValue<IsNotQueryContext>>(content),
-                (0x4541, true)  => CreateValue<AEValue<IsQueryContext>>(content),
-                (0x5351, _)     => CreateValue<ASValue>(content),
+                (0x4541, true) => CreateValue<AEValue<IsQueryContext>>(content),
+                (0x5351, _) => CreateValue<ASValue>(content),
                 (0x4144, false) => CreateValue<DAValue<IsNotQueryContext>>(content),
-                (0x4144, true)  => CreateValue<DAValue<IsQueryContext>>(content),
-                _               => throw new NotImplementedException(),
+                (0x4144, true) => CreateValue<DAValue<IsQueryContext>>(content),
+                _ => throw new NotImplementedException(),
             };
 
             static AbstractValue CreateValue<TValue>(T content) where TValue : struct, IValue<TValue>
@@ -36,11 +36,11 @@ namespace DiCor
             => (_value, isQueryContext) switch
             {
                 (0x4541, false) => value.As<AEValue<IsNotQueryContext>>().Get<T>(),
-                (0x4541, true)  => value.As<AEValue<IsQueryContext>>().Get<T>(),
-                (0x5351, _)     => value.As<ASValue>().Get<T>(),
+                (0x4541, true) => value.As<AEValue<IsQueryContext>>().Get<T>(),
+                (0x5351, _) => value.As<ASValue>().Get<T>(),
                 (0x4144, false) => value.As<DAValue<IsNotQueryContext>>().Get<T>(),
-                (0x4144, true)  => value.As<DAValue<IsQueryContext>>().Get<T>(),
-                _               => throw new NotImplementedException(),
+                (0x4144, true) => value.As<DAValue<IsQueryContext>>().Get<T>(),
+                _ => throw new NotImplementedException(),
             };
     }
 }
